a7bafd479710a2c754d637042360a851a154a3ea
Aarón J. Morales Botton <aaron@midokura.com>
2022-05-04 23:31:43 +0200

    AssetID cannot be generated in business
    Pending to refactor the entire codebase

--- chaincode/pig/cage-contract.go
+++ chaincode/pig/cage-contract.go
@@ -10,6 +10,18 @@ func (c *PigContract) CageExists(ctx contractapi.TransactionContextInterface, ca
 	return c.EntityExists(ctx, cageId)
 }
 
+func (c *PigContract) CreateCageID(ctx contractapi.TransactionContextInterface, name string, id string) error {
+	_, err := c.CageExists(ctx, id)
+	if err != nil {
+		return fmt.Errorf(error_cage_aleady_exists, id)
+	}
+	cage := Cage{
+		Name: name,
+	}
+	cageBytes, _ := json.Marshal(cage)
+	return ctx.GetStub().PutState(id, cageBytes)
+}
+
 func (c *PigContract) CreateCage(ctx contractapi.TransactionContextInterface, name string) error {
 	id, err := c.generateID(ctx)
 	if err != nil {
