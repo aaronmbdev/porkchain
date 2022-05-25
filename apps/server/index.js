7c83c7b85a97760677854566e48fec87a5129e31
Aarón J. Morales Botton <aaron@midokura.com>
2022-05-25 15:23:08 +0200

    Server in progress

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const meatchain = require('./service/MeatchainService');
 const utils = require("./utils/AppUtils");
@@ -10,6 +11,20 @@ const utils = require("./utils/AppUtils");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const allowedOrigins = ['http://localhost:3000',
+  'https://farm.meatchain.cloud','https://factory.meatchain.cloud','https://market.meatchain.cloud'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let msg = 'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
